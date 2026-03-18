2159e44e344aaddf5b6c55c07ebd826b7c015d10
ALTIORACORP\dalpala <Diego Alpala>
2026-03-18 11:30:57 -0500

    feat: se finaliza mejora en reportes de pnc para tomar las imagenes del mismo proyecto

--- src/main/java/com/isacore/servicio/reporte/GeneradorJasperReportsImpl.java
+++ src/main/java/com/isacore/servicio/reporte/GeneradorJasperReportsImpl.java
@@ -1,21 +1,20 @@
 package com.isacore.servicio.reporte;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Map;
-
+import com.isacore.exception.reporte.JasperReportsException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import com.isacore.exception.reporte.JasperReportsException;
-
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Map;
 
 @Service
 public class GeneradorJasperReportsImpl implements IGeneradorJasperReports {
@@ -30,8 +29,8 @@ public class GeneradorJasperReportsImpl implements IGeneradorJasperReports {
 
 		try {
 			final String ruta = prepararReporteRuta(reporteNombre);
-
-			JasperPrint print = JasperFillManager.fillReport(ruta, null,
+			this.cargarParametrosBase(parametrosEspecificos);
+			JasperPrint print = JasperFillManager.fillReport(ruta, parametrosEspecificos,
 					new JRBeanCollectionDataSource(objetos));
 
 			LOG.info("FINALIZA creacion de documento JASPER");
@@ -51,6 +50,22 @@ public class GeneradorJasperReportsImpl implements IGeneradorJasperReports {
 		final File file = new ClassPathResource(ruta).getFile();
 		LOG.info("Ruta del reporte: " + ruta);
 		return file.getPath();
+	}
+
+	private void cargarParametrosBase(Map<String, Object> parametrosBase){
+		parametrosBase.put(ReporteConstantes.LOGO_PARAMETRO, getImagenStream(ReporteConstantes.LOGO_IMAGEN));
+		parametrosBase.put(ReporteConstantes.FONDO_PARAMETRO, getImagenStream(ReporteConstantes.FONDO_IMAGEN));
+		parametrosBase.put(ReporteConstantes.SELLO_CALIDAD_PARAMETRO, getImagenStream(ReporteConstantes.SELLO_CALIDAD_IMAGEN));
+	}
+
+	private InputStream getImagenStream(String imageName) {
+		String imagenBase = "/images/";
+		InputStream is = this.getClass().getResourceAsStream(imagenBase + imageName);
+		if (is == null) {
+			// Opcional: devolver una imagen por defecto o lanzar excepción
+			throw new IllegalArgumentException("Recurso de imagen no encontrado: " + imageName);
+		}
+		return is;
 	}
 
 }
